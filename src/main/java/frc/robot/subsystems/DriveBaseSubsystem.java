a1e056cd44a5c26c7052001c848d39e1e3602fa6
Brandon Martinez <me@brandonmartinez.com>
2025-01-15 18:53:33 -0500

    Switched to joystick for input

--- src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
@@ -21,7 +21,6 @@ import java.io.File;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
