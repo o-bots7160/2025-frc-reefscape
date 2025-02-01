fce270c9fd01d6a4852215c4ccbf6ac3bd324f99
Jaiden Rhodes <jdrhodes18@icloud.com>
2025-02-01 12:57:14 -0500

    Added pathplanner setup

--- src/main/java/frc/robot/AllianceLandmarks.java
+++ src/main/java/frc/robot/AllianceLandmarks.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class AllianceLandmarks
--- src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveBaseSubsystem.java
@@ -10,17 +10,23 @@ import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import java.io.File;
 import java.util.function.DoubleSupplier;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -69,6 +75,9 @@ public class DriveBaseSubsystem extends SubsystemBase
     NetworkTableEntry tl      = table.getEntry("tl");
     NetworkTableEntry cl      = table.getEntry("cl");
     NetworkTableEntry botpose = table.getEntry("botpose");
+
+    RobotConfig robotConfig;
+
     //
     //  Command to manually move the robot using joysticks field oriented
     //
@@ -129,6 +138,7 @@ public class DriveBaseSubsystem extends SubsystemBase
                     0.1);
             swerveController.thetaController.setPID(driveBaseSubsystemConfig.thetaControllerPidKp,
                     driveBaseSubsystemConfig.thetaControllerPidKi, driveBaseSubsystemConfig.thetaControllerPidKd);
+            robotConfig = RobotConfig.fromGUISettings();
         }
         catch (Exception e)
         {
@@ -145,6 +155,32 @@ public class DriveBaseSubsystem extends SubsystemBase
 
         xy_profile = new TrapezoidProfile(new TrapezoidProfile.Constraints( driveBaseSubsystemConfig.maximumSpeedInMeters(), 0.75)); // TODO: Max linear accel?
         swerveDrive.setMotorIdleMode(true);
+
+        AutoBuilder.configure(
+            this::getPose, // Robot pose supplier
+            this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+            this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            (speeds, feedforwards) -> swerveDrive.drive(speeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+                                                        swerveDrive.kinematics.toSwerveModuleStates(speeds), 
+                                                        feedforwards.linearForces()), 
+            new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+                    new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                    new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+            ),
+            robotConfig, // The robot configuration
+            () -> {
+              // Boolean supplier that controls when the path will be mirrored for the red alliance
+              // This will flip the path being followed to the red side of the field.
+              // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+              var alliance = DriverStation.getAlliance();
+              if (alliance.isPresent()) {
+                return alliance.get() == DriverStation.Alliance.Red;
+              }
+              return false;
+            },
+            this // Reference to this subsystem to set requirements
+        );
     }
     /**
     * Called once per timeslice
@@ -334,7 +370,7 @@ public class DriveBaseSubsystem extends SubsystemBase
         xy_speed = new Translation2d( x, y );
         setTarget( new Rotation2d( Math.atan2( xy_target.getY() - current_pose.getY(),
                                                xy_target.getX() - current_pose.getX() ) ),
-                                    current_pose.getRotation() );
+                                   current_pose.getRotation() );
         if ( SetRSpeedFromTarget( current_pose.getRotation() ) )
         {
             haveTarget = true;
@@ -434,6 +470,16 @@ public class DriveBaseSubsystem extends SubsystemBase
         return swerveDrive.getPose();
     }
 
+    public void resetPose(Pose2d new_pose)
+    {
+        swerveDrive.resetOdometry(new_pose);
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds()
+    {
+        return swerveDrive.getRobotVelocity();
+    }
+
     /**
     * Sets the current pose of the robot from odometery (usually at the start of
     * auton)
@@ -498,7 +544,7 @@ public class DriveBaseSubsystem extends SubsystemBase
             y         = new_y;
             r         = new_r;
             driveBase = subsystem;
-            addRequirements( driveBase);
+            addRequirements( driveBase );
         }
 
         // Called every time the scheduler runs while the command is scheduled.
